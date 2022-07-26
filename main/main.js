1cb03441159f08156c4b2876ce7d86c55c1aba84
DeadCloud <pavel2012ig@gmail.com>
2022-07-26 13:24:51 +0300

    added all file

---
+++ main/main.js
@@ -0,0 +1,113 @@
+
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 11,
+  spaceBetween: 190,
+  rewind: true,
+  mousewheel: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (2022 - index) + "</span>";
+    },
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 11,
+      direction: "vertical",
+      spaceBetween: 1,
+      centeredSlides: true,
+      
+    },
+    640: {
+      direction: "vertical",
+      spaceBetween: 1, 
+    },
+    820: {
+      direction: "horizontal",
+      slidesPerView: 11,
+      spaceBetween: 60,
+      centeredSlides: true,
+    },
+    1440: {
+      centeredSlides: true,
+      slidesPerView: 11,
+      spaceBetween: 120,
+    },
+  },
+  // Install Plugin To Swiper
+  modules: [myPlugin]
+});
+
+function myPlugin({ swiper, extendParams, on }) {
+  extendParams({
+    debugger: false,
+  });
+
+  on('init', () => {
+    if (!swiper.params.debugger) return;
+    console.log('init');
+  });
+  on('click', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('click');
+  });
+  on('tap', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('tap');
+  });
+  on('doubleTap', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('doubleTap');
+  });
+  on('sliderMove', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('sliderMove');
+  });
+  on('slideChange', () => {
+    if (!swiper.params.debugger) return;
+    console.log(
+      'slideChange',
+      swiper.previousIndex,
+      '->',
+      swiper.activeIndex
+    );
+  });
+  on('slideChangeTransitionStart', () => {
+    if (!swiper.params.debugger) return;
+    console.log('slideChangeTransitionStart');
+  });
+  on('slideChangeTransitionEnd', () => {
+    if (!swiper.params.debugger) return;
+    console.log('slideChangeTransitionEnd');
+  });
+  on('transitionStart', () => {
+    if (!swiper.params.debugger) return;
+    console.log('transitionStart');
+  });
+  on('transitionEnd', () => {
+    if (!swiper.params.debugger) return;
+    console.log('transitionEnd');
+  });
+  on('fromEdge', () => {
+    if (!swiper.params.debugger) return;
+    console.log('fromEdge');
+  });
+  on('reachBeginning', () => {
+    if (!swiper.params.debugger) return;
+    console.log('reachBeginning');
+  });
+  on('reachEnd', () => {
+    if (!swiper.params.debugger) return;
+    console.log('reachEnd');
+  });
+}
